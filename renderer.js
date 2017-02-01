f5b0fc3c51f9d5152a3ed59817d6e4fb36168c32
jfswitz@gmail.com <jfswitz@gmail.com>
2017-02-01 21:05:05 +0100

    add persistant storage

--- renderer.js
+++ renderer.js
@@ -2,6 +2,16 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process
 
+function init(){
+	var Storage = require('node-storage');
+	var store = new Storage('db.json');
+
+	//store.put('hello', 'world');
+
+	console.log(store.get('hello'));
+}
+
+init()
 
 window.setInterval(function(){
 	console.log("it works");
