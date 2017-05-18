451f8b45ec4746d585fa52215cd150446865a40e
John Francis <jfswitz@gmail.com>
2017-05-18 23:51:43 +0200

    I don't remember what i did... so yolo

--- controllers/hue.js
+++ controllers/hue.js
@@ -1,27 +1,23 @@
 var hue = require("node-hue-api");
+var HueApi = hue.HueApi;
 
 var displayBridges = function(bridge) {
     console.log("Hue Bridges Found: " + JSON.stringify(bridge));
 };
 
-// Search for bridge
-// hue.nupnpSearch(function(err, result) {
-//     if (err) throw err;
-//     displayBridges(result);
-// });
-
+var getBridge = function(req, res, next) {
+  // get bridge
+  hue.nupnpSearch()
+  .then(function (output) {
+    res.json(output.toJSON());
+  })
+  .otherwise(function (error) {
+    res.status(500).json({msg: error.message});
+  })
+  .done();
+};
 
 module.exports = {
   // Search for bridge on network
-  getBridge: function(req, res, next) {
-    // get bridge
-    hue.nupnpSearch()
-    .then(function (output) {
-      res.json(output.toJSON());
-    })
-    .otherwise(function (error) {
-      res.status(500).json({msg: error.message});
-    })
-    .done();
-  },
+  getBridge: getBridge,
 }
--- js/setuppage.js
+++ js/setuppage.js
@@ -1,19 +1,39 @@
 var hue = require("node-hue-api"),
     timeout = 2000; // 2 seconds
 
+var HueApi = new hue.HueApi();
+var FOUNDBRIDGE= null;
+
+var displayUserResult = function(result) {
+      console.log("Created user: " + JSON.stringify(result));
+};
+
+var displayError = function(err) {
+      console.log(err);
+};
+
+var registerUser = function(host, userDescription) {
+  HueApi.registerUser(host, userDescription)
+      .then(displayUserResult)
+      .fail(displayError)
+      .done();
+};
+
+
 var handleBridges = function(bridges) {
-    console.log("Hue Bridges Found: " + JSON.stringify(bridges));
-    if (bridges.length == 0) {
-      $("#message").text("Oops, I couldn't find a hue bridge on your network.");
-      $("#manual_bridge_ip").show();
-      $("#bridge_scan").text("Search again");
-    }
-    else {
-      var myBridge = bridges[0];
-      $("#message").text("Ok, I found a bridge on your network with the IP " + myBridge["ipaddress"]);
-      $("#continue").css("visibility", "visible");
-      $("#bridge_scan").css("visibility", "hidden");
-    }
+  console.log("Hue Bridges Found: " + JSON.stringify(bridges));
+  FOUNDBRIDGE = bridges[0];
+
+  if (bridges.length == 0) {
+    $("#message").text("Oops, I couldn't find a hue bridge on your network.");
+    $("#manual_bridge_ip").show();
+    $("#bridge_scan").text("Search again");
+  }
+  else {
+    $("#message").text("Ok, I found a bridge on your network with the IP " + bridges[0]["ipaddress"]);
+    $("#bridge_register").css("visibility", "visible");
+    $("#bridge_scan").css("visibility", "hidden");
+  }
 };
 
 
@@ -24,6 +44,19 @@ $( document ).ready(function() {
     hue.upnpSearch(timeout).then(handleBridges).done();
     $("#message").text("Searching for bridges on your network...");
   });
+  
+  $("#bridge_register").click(function() {
+    console.log("Attempting to register hue user to the hue bridge.");
+    //registerUser(FOUNDBRIDGE["ipaddress"], "Chromaticity"); 
+    HueApi.createUser(FOUNDBRIDGE["ipaddress"], function(err, user) {
+        if (err) {
+          $("#message").text("Oh no, I couldn't connect to the bridge. The link button wasn't pressed.");
+        } else {
+          displayUserResult(user);
+          $("#bridge_register").css("visibility", "hidden");
+          $("#message").text("Hooray, I was able to successfully connect to the bridge");
+        };
+    });
+    
+  });
 });
-
-
