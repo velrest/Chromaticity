431e9445e91d4442c1de535b2a60b45c282855cd
John Francis <jfswitz@gmail.com>
2017-10-29 22:57:01 +0100

    add table of users

--- js/main.js
+++ js/main.js
@@ -10,11 +10,26 @@ var host = store.get("bridge_ip"),
 
 api = new hue.HueApi(host, username);
 
+var generateUserList = function(object) {
+  html = ""
+  listelem = "<li>{0} - {1}</li>"
+    for (i=0;i<object.devices.length;i++) {
+    html += "<tr><td>" + object.devices[i].name + "</td>" + 
+        "<td>" + object.devices[i].username + "</td>" +
+        "<td>" + object.devices[i].created + "</td>" +
+        "<td>" + object.devices[i].accessed + "</td></tr>"; 
+  }
+  document.getElementById("userlist").innerHTML = html;
+};
+
+
 var initialize_page = function() {
   console.log("BRIDGE CONFIGURATION")
   console.log(api.fullState(console.log));
   console.log("ALL USERS")
   console.log(api.registeredUsers(console.log));
+  api.registeredUsers().then(generateUserList).done()
+  
 };
 
 initialize_page();
